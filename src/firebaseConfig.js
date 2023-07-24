be59c1efb0c7e1a126d0ed4b321f97f454eedfdd
pd212 <74095344+pd212@users.noreply.github.com>
2023-07-24 10:47:03 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 
 console.log(process.env.REACT_APP_API_KEY);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+      apiKey: "AIzaSyBCExZp5vK3Mo1RTiUS5bVl4OVatYy_kvw",
+    authDomain: "typing-speed-test-44f80.firebaseapp.com",
+    projectId: "typing-speed-test-44f80",
+    storageBucket: "typing-speed-test-44f80.appspot.com",
+    messagingSenderId: "958077196154",
+    appId: "1:958077196154:web:5ad33533dd6f862d85a8c3",
+    measurementId: "G-CMYMV93VN7"
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
